8460484e17b80efeedaeb37968ed1633da30b55a
Vy <vyom.j@protonmail.com>
2024-08-02 13:40:26 +0530

    pass stage

--- src/main/kotlin/ErrorHandler.kt
+++ src/main/kotlin/ErrorHandler.kt
@@ -1,7 +1,12 @@
-fun error(line: Int, message: String) {
-    report(line, "", message)
-}
 
-private fun report(line: Int, where: String, message: String) {
-    System.err.println("[line $line] Error$where: $message")
+object ErrorHandler {
+    var hadError = false;
+    fun error(line: Int, message: String) {
+        report(line, "", message)
+    }
+
+    private fun report(line: Int, where: String, message: String) {
+        System.err.println("[line $line] Error$where: $message")
+        hadError = true;
+    }
 }
